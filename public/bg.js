9f2ca62e3b8b921c4ffb96941f74a03b280383c6
Jared <jarednkrause@gmail.com>
2020-11-27 13:34:46 -0500

    three.js background

---
+++ public/bg.js
@@ -0,0 +1,36 @@
+var scene = new THREE.Scene();
+scene.background = new THREE.Color("#e6eaff");
+var camera = new THREE.PerspectiveCamera(-40, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.SphereGeometry(5, 10, 10);
+var material = new THREE.MeshBasicMaterial({ color: "#ffffff", wireframe: true });
+var cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+camera.position.z = 6;
+
+var animate = function () {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.001;
+  cube.rotation.y += 0.001;
+
+  renderer.render(scene, camera);
+};
+
+animate();
+
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view/>
+    <div class="app-inner" ref="appInner">
+      <!-- <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div> -->
+      <router-view/>
+    </div>
   </div>
 </template>
+<script>
+import Vue from 'vue';
+// import THREELib from "three-js";
+// const THREE = THREELib();
 
+export default Vue.extend({
+  name: 'App',
+  // data () {
+  //   return {
+  //     scene: new THREE.Scene(),
+  //     camera: new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ),
+  //     renderer: new THREE.WebGLRenderer()
+  //   }
+  // },
+  // components: {
+  // },
+  // created: function () {
+  //   Vue.nextTick(()=> {
+  //     console.info(this.$refs.appInner.offsetWidth, this.$refs.appInner.offsetHeight)
+  //     this.renderer.setSize( this.$refs.appInner.offsetWidth, this.$refs.appInner.offsetHeight );
+  //     this.$refs.appInner.append( this.renderer.domElement );
+  //     const geometry = new THREE.BoxGeometry();
+  //     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  //     const cube = new THREE.Mesh( geometry, material );
+  //     this.scene.add( cube );
+
+  //     this.camera.position.z = 5;
+  //     this.animate();
+  //   });
+  // },
+  // methods: {
+  //   animate: function () {
+  //     requestAnimationFrame( this.animate );
+  //     this.renderer.render( this.scene, this.camera );
+  //   }
+  // }
+});
+</script>
 <style src="@/assets/styles/main.styl" lang="stylus"></style>
 

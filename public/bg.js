17002db003c5ba48f62f570337c4191069ee1b55
Jared <jarednkrause@gmail.com>
2020-11-27 16:22:13 -0500

    changes to colors. simplify

--- public/bg.js
+++ public/bg.js
@@ -8,37 +8,18 @@ document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.SphereGeometry(4, 10, 10);
 var material = new THREE.MeshBasicMaterial({
-  color: "#ffffff",
+  color: "#f9844a",
   wireframe: true
 });
 var sphere = new THREE.Mesh(geometry, material);
 
 scene.add(sphere);
 camera.position.z = 6;
-var colors = [
-  "#f94144",
-  "#f3722c",
-  "#f8961e",
-  "#f9844a",
-  "#90be6d",
-  "#43aa8b",
-  "#4d908e",
-  "#577590",
-  "#277da1"]
 
-var index = 0;
 var animate = function () {
   requestAnimationFrame(animate);
-  var num = Math.floor(index / 400) % (colors.length - 1)
-  material.color.set(colors[num]);
-  index++;
-
-  sphere.rotation.x += 0.0005; // * ((num / 8) + 0.5);
-  sphere.rotation.y += 0.0005; // * ((num / 8) + 0.5);
-
-  // sphere2.rotation.x += -0.001;
-  // sphere2.rotation.y += -0.002;
-
+  sphere.rotation.x += 0.0005;
+  sphere.rotation.y += 0.0005;
   renderer.render(scene, camera);
 };
 

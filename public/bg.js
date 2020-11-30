4906a8d67c66b51c43929088fe98c60224c75df4
Jared <jarednkrause@gmail.com>
2020-11-30 11:50:31 -0500

    styles

--- public/bg.js
+++ public/bg.js
@@ -8,7 +8,8 @@ document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.SphereGeometry(4, 10, 10);
 var material = new THREE.MeshBasicMaterial({
-  color: "#fcc1a4",
+  // color: "#fcc1a4",
+  color: "#f2dcb8",
   wireframe: true
 });
 var sphere = new THREE.Mesh(geometry, material);

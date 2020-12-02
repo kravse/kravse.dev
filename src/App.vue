3409c97c4ce41771a65c6740d88d8bda859173fb
Jared <jarednkrause@gmail.com>
2020-12-02 17:30:19 -0500

    try a thing

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,42 @@ import notification from '@/components/notification/notification.vue'
 import theylive from '@/components/theylive/theylive.vue'
 
 export default Vue.extend({
+  data() {
+    return {
+      // strings: [
+      //   "Hello there",
+      //   "HEllo there",
+      //   "HELlo there",
+      //   "HELLo there",
+      //   "HELLO there"
+      // ]
+    }
+  },
   name: 'App',
   components: {
     'site-footer': footer,
     'site-notification': notification,
     'they-live': theylive
+  },
+  created: function () {
+    // this.consoleArt()
+  },
+  methods: {
+    // consoleArt: function () {
+    //   let i = 0;
+    //   setInterval(() => {
+    //     i = (i+1) % this.strings.length
+    //     console.clear();
+    //     console.log(`%c${this.strings[i]}`, `
+    //       background: #f7f6f1;
+    //       border: 3px solid #e07a5f;
+    //       color: #4d7963;
+    //       font-size: 50px;
+    //       margin: 40px;
+    //       padding: 20px;
+    //     `);
+    //   }, 500)
+    // }
   },
   computed: {
     overlay: function (): boolean {

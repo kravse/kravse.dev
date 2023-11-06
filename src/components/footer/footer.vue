bb5b1a6c3b44a296adf48972a50b599f93bfabe6
Jared Krause <kravse@users.noreply.github.com>
2023-11-06 15:13:41 -0500

    Update footer.vue

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -3,7 +3,6 @@
     <section>
       <p>
         <a href="https://www.github.com/kravse">Github</a>
-        <a rel="me" href="https://infosec.exchange/@kravse">Mastodon</a>
         <a href="https://www.kravse.com">Photos</a>
         <a href="https://www.linkedin.com/in/jarednkrause/">LinkedIn</a>
         <a :href="`mailt`+`o:jare`+`d@k`+`ravs`+`e.de`+`v`">{{`jare`+`d@k`+`rav`+`se.`+`de`+`v`}}</a>
